baa0e2a6d13aaa8c5a200e642ffab9cc93e76361
Yuhei Nakata <yuh.nakata@gmail.com>
2019-10-30 16:30:45 +0100

    update home

--- apps/demo-yuhei/src/app/container/home.tsx
+++ apps/demo-yuhei/src/app/container/home.tsx
@@ -10,31 +10,28 @@ import {Footer} from '@sqli/gui'
 import {Autocomplete} from '@sqli/gui'
 
 export const Home = ({store}) => {
-  /*
-   * Replace the elements below with your own.
-   *
-   * Note: The corresponding styles are in the ./${fileName}.${style} file.
-   * 
-   */
 
-   const [loaded, setLoaded] = React.useState(false);
-   const [tracks, setTracks] = React.useState([]);
-   const [users, setUsers] = React.useState([]);
+    const [loaded, setLoaded] = React.useState(false);
+    const [tracks, setTracks] = React.useState([]);
+    const [users, setUsers] = React.useState([]);
+    const [selectedTrack, setSelectedTrack] = React.useState(null);
 
-   const userToMenuItem = user => ({label:user.name});
-   const trackToTextItem = track => (
-       {
+    const userToMenuItem = user => ({label:user.name});
+
+    const textButtonAction = () => {};
+    const trackToTextItem = track => 
+       ({
            title: track.name,
            subtitle: track.album.name,
            imageSrc: track.album.image,
            href: track.href,
-           ctaTitle: <button>Play</button>,
+           ctaTitle: <Button action={textButtonAction}>Play</Button>,
            content: track.name
-        }
-    );
+        });
+    const navigate = data => {console.log(data)};
 
-   // access fonctionnel au cycle de vie
-   React.useEffect(()=>{
+    // access fonctionnel au cycle de vie
+    React.useEffect(()=>{
         const requestUser = new store.Action(store.ActionTypes.USER_GET_LIST, null);
         const requestTrack = new store.Action(store.ActionTypes.TRACK_GET_LIST, null);
         store.dispatch(requestUser).then(setUsers);
@@ -44,60 +41,27 @@ export const Home = ({store}) => {
 
         // clean up function à faire sur le unmount
        return ()=> console.log('Bye Bye');
-   }, [loaded]);
+    }, [loaded]);
 
-  const data = ['France','Morocco', 'usa', 'Canada'];
-
-  const navigate = data => {
-    console.log(data);
-    };
-
-  const menu = [
-    { label: 'Lien 1', action: navigate },
-    { label: 'Lien 2', action: navigate },
-    { label: 'Lien 3', action: navigate },
-    { label: 'Lien 4', action: navigate }
-  ];
-  const trail = [
-    {
-        action:'http://www.rorymcilroy.com/',
-        label:'Rory Mcilroy'
-    },
-    {
-        action:'https://twitter.com/kikxmachine',
-        label:'Superkikx'
-    },
-    {
-        action:'https://tigerwoods.com/',
-        label:'Tiger Woods'
-    }
-];
-    const trackCardHandler = data => {
-        console.log(data);
-    };
-
-    
+  
 
   return (
     <div className="app">
       <Logo size="small"/>
       <MenuSofiane>{users.map(userToMenuItem)}
       </MenuSofiane>
-      <BreadcrumbSqli>{trail}</BreadcrumbSqli>
-      <AudioPlayer coverImage="https://www.fishipedia.fr/wp-content/themes/fishipedia/css/img/home/thematics/fishes.jpg"/>
-      <Autocomplete data={data} placeholder="e.g. Denmark"/>
-      <Button action={()=>'ok'}>Lancer la recherche</Button>
-
-    <div className="cards-list">
-      {tracks.map( track => (
-          <Text>{trackToTextItem(track)}</Text>
-      ))}
+      <BreadcrumbSqli>{[]}</BreadcrumbSqli>
+      <AudioPlayer/>
+      <Autocomplete data={tracks.map(track => track.name)} placeholder="e.g. Light up"/>
+      <Button action={()=>'ok'}><h1>Lancer la recherche</h1></Button>
+        <div className="cards-list">
+        {tracks.map( (track, num) => (
+          <Text key={num}>{trackToTextItem(track)}</Text>
+        ))}
+        </div>
+        <Footer background="gray"></Footer>
     </div>
-
-      <Footer background="gray"></Footer>
-      </div>
   );
 };
-
 
 export default Home;
